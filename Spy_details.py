8cb60e80a3e0700a9e3bf9345ba0a1967b9eca43
shaluaggarwal64423 <37615919+shaluaggarwal64423@users.noreply.github.com>
2018-04-11 22:26:20 +0530

    Update Spy_details.py

--- Spy_details.py
+++ Spy_details.py
@@ -13,12 +13,9 @@ class Spy:
 
 spy = Spy('Shalu', 'Miss.', 20, 3.5)
 
-friend_one = Spy('Deepanshu', 'Mr.', 20, 4.7)
-friend_two = Spy('Tanya', 'Ms.', 20, 4.8)
-friend_three = Spy('Rahul', 'Mr.', 20, 4.8)
-friend_four = Spy('Ritesh', 'Mr..', 21, 4.9)
-friend_five = Spy('Harshika', 'Ms.', 20, 4.9)
-friend_six = Spy('Yash', 'Mr..', 20, 4.9)
-
-
-friends = [friend_one, friend_two, friend_three,friend_four,friend_five,friend_six]
+# chat class
+class ChatMessage:
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+        self.time = datetime.now()
